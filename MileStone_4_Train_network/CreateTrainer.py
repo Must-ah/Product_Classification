d27dcf687fa69663c6b7797fdb4064fccba1c347
must-ah <Mustafa.Abdel-Hussein@hotmail.com>
2021-10-01 13:25:08 +0200

    removed Events from TrainingIgnite and merged Events in CreateTrainer

--- MileStone_4_Train_network/CreateTrainer.py
+++ MileStone_4_Train_network/CreateTrainer.py
@@ -1,14 +1,20 @@
 import torch
-from ignite.metrics import Accuracy, Loss, RunningAverage
+from ignite.contrib.engines.common import save_best_model_by_val_score
+from ignite.metrics import Accuracy, Loss, RunningAverage, ClassificationReport
 from ignite.handlers import ModelCheckpoint, Checkpoint, DiskSaver
 from ignite.contrib.handlers import ProgressBar
+# from ignite.metrics.precision import Precision
+# from ignite.metrics.recall import Recall
+
 from pathlib import Path
-from ignite.engine import Engine, Events
+from ignite.engine import Engine, Events, create_supervised_evaluator
 # source https://colab.research.google.com/github/pytorch/ignite/blob/master/assets/tldr/teaser.ipynb#scrollTo=dFglXKeKOgdW
 
 
-def create_trainer(model, optimizer, criterion, lr_scheduler, config):
+def create_trainer(model, optimizer, criterion, lr_scheduler, config, data):
     device = config['device']
+    (train_loader, train_sampler), (valid_loader,
+                                    valid_sampler), (test_loader, test_sampler) = data
     # Define any training logic for iteration update
 
     def train_step(engine, batch):
@@ -42,45 +48,57 @@ def create_trainer(model, optimizer, criterion, lr_scheduler, config):
     validation_evaluator = Engine(eval_model_on_batch)
     test_evaluator = Engine(eval_model_on_batch)
 
+    evaluator = create_supervised_evaluator(
+        model,
+        metrics={"accuracy": Accuracy(),
+                 "loss": RunningAverage(Accuracy()),
+                 #  "precision": Precision(average=False),
+                 #  'recall': Recall(average=False),
+                 'cr': ClassificationReport(output_dict=True, is_multilabel=False)
+                 },
+        device=config.get('device', 'cpu')
+    )
+
     @trainer.on(Events.ITERATION_COMPLETED(every=10))
     def save_checkpoint():
         fp = Path(config.get("output_path", "./output")) / "checkpoint.pt"
         torch.save(model.state_dict(), fp)
 
-    # @trainer.on(Events.EPOCH_COMPLETED)
-    # def log_training_results(engine):
-    #     train_evaluator.run(train_loader)
-    #     metrics = train_evaluator.state.metrics
-    #     avg_accuracy = metrics['accuracy']
-    #     avg_nll_loos = metrics['NLLLoss']
-    #     pbar.log_message(
-    #         "Training Results - Epoch: {} Avg accuracy: {:.2f} avg loss {:.2f}".format(engine.state.epoch, avg_accuracy,
-    #                                                                                    avg_nll_loos)
-    #     )
+    pbar = ProgressBar(persist=True, bar_format="")
 
-    # @trainer.on(Events.EPOCH_COMPLETED)
-    # def log_validation_results(engine):
-    #     validation_evaluator.run(valid_loader)
-    #     metrics = validation_evaluator.state.metrics
-    #     avg_accuracy = metrics['accuracy']
-    #     avg_nll_loos = metrics['NLLLoss']
-    #     pbar.log_message
-    #     (
-    #         "Training Results - Epoch: {} Avg accuracy: {:.2f} avg loss {:.2f}".format(
-    #             engine.state.epoch, avg_accuracy, avg_nll_loos)
-    #     )
-    #     pbar.n = pbar.last_print_n = 0
+    # Run model evaluation every 3 epochs and show results
+    @trainer.on(Events.EPOCH_COMPLETED(every=1))
+    def evaluate_model():
+        state = evaluator.run(valid_loader)
+        metrics = state.metrics
+        acc = metrics['accuracy']
+        loss = metrics['loss']
+        # recAll = metrics['recall']
+        # preciss = metrics['precision']
+        # F1 = preciss * recAll * 2 / (preciss + recAll + 1e-20)
+        # F1 = MetricsLambda(lambda t: torch.mean(t).item(), F1)
+        res_dic = metrics['cr']
+        # res_dic = ast.literal_eval(res)
+        train_evaluator.run(train_loader)
+        validation_evaluator.run(valid_loader)
 
-    # Add progress bar showing batch loss value
+        pbar.log_message(
+            "Validation set: Average loss: {:.4f}, Accuracy: {:.4f} F1 for class {} is {:.4f}".format(
+                loss, acc, 14, res_dic['14']['f1-score'])
+        )
+
+    RunningAverage(output_transform=lambda x: x).attach(
+        trainer, 'loss'
+    )
+    pbar.attach(trainer, ['loss'])
+    pbar.attach(trainer, output_transform=lambda x: {"batch loss": x})
+
     Accuracy(output_transform=lambda x: x).attach(train_evaluator, 'train_acc')
     Loss(criterion).attach(train_evaluator, 'train_NLLLoss')
 
     Accuracy(output_transform=lambda x: x).attach(
         validation_evaluator, 'val_acc')
     Loss(criterion).attach(validation_evaluator, 'val_NLLLoss')
-
-    RunningAverage(output_transform=lambda x: x).attach(
-        trainer, 'loss')
 
     checkpointer = ModelCheckpoint(
         config.get('checkpointPath', './tmp/models'), config.get('model',
@@ -89,11 +107,7 @@ def create_trainer(model, optimizer, criterion, lr_scheduler, config):
         save_as_state_dict=True, require_empty=config.get('require_empty', True)
     )
 
-    pbar = ProgressBar(persist=True, bar_format="")
-    pbar.attach(trainer, ['loss'])
-    pbar.attach(trainer, output_transform=lambda x: {"batch loss": x})
-
-    trainer.add_event_handler(Events.EPOCH_COMPLETED,
+    trainer.add_event_handler(Events.EPOCH_COMPLETED(every=3),
                               checkpointer, {config.get('model'): model}
                               )
     to_save = {'trainer': trainer,
@@ -107,6 +121,14 @@ def create_trainer(model, optimizer, criterion, lr_scheduler, config):
                                    create_dir=True, require_empty=config['require_empty']
                                    )
                          )
+    bestModelSaver = save_best_model_by_val_score(output_path=config['best_model_path'],
+                                                  evaluator=evaluator, model=model,
+                                                  metric_name='accuracy',
+                                                  n_saved=config.get(
+                                                      "best_n_saved", 2)
+                                                  )
+    evaluator.add_event_handler(Events.EPOCH_COMPLETED, bestModelSaver)
+
     trainer.add_event_handler(Events.EPOCH_COMPLETED, handler)
 
-    return trainer, train_evaluator, validation_evaluator, test_evaluator, pbar
+    return trainer, train_evaluator, validation_evaluator, test_evaluator, evaluator, pbar
--- MileStone_4_Train_network/TrainingIgnite.py
+++ MileStone_4_Train_network/TrainingIgnite.py
@@ -1,16 +1,14 @@
 from InitializeModel import initializeModel
 from CreateTrainer import create_trainer
 from getData import getData
-from ignite.engine import create_supervised_evaluator, Events
-from ignite.metrics import Accuracy, Loss, RunningAverage
+#from ignite.engine import create_supervised_evaluator, Events
+# from ignite.metrics import Accuracy, Loss, RunningAverage
 from ignite.contrib.engines import common
-from ignite.metrics.precision import Precision
-from ignite.metrics.recall import Recall
-from ignite.handlers import EarlyStopping
-from ignite.metrics import ClassificationReport
-from ignite.contrib.engines.common import save_best_model_by_val_score
+# from ignite.handlers import EarlyStopping
+# from ignite.metrics import ClassificationReport
 import ignite.distributed as idist
-import torch
+# import torch
+
 # source https://colab.research.google.com/github/pytorch/ignite/blob/master/assets/tldr/teaser.ipynb#scrollTo=dFglXKeKOgdW
 # dependenciues
 
@@ -21,103 +19,19 @@ import torch
 
 def training(config):
     # Setup dataflow and
-    (train_loader, train_sampler), (valid_loader,
-                                    valid_sampler), (test_loader, test_sampler) = getData(config)
-
+    data = getData(config)
     model, optimizer, criterion, lr_scheduler = initializeModel(config)
-
     model.to(config.get('device', 'cpu'))
-
     # Setup model trainer and evaluator
-    trainer, train_evaluator, validation_evaluator, test_evaluator, pbar = create_trainer(
-        model, optimizer, criterion, lr_scheduler, config)
-    if config['train_now'] is not True:
-        return trainer, model, optimizer,criterion ,lr_scheduler, train_loader
-
-    def thresholded_output_transform(output):
-        y_pred, y = output
-        y_pred = torch.round(y_pred)
-        return y_pred, y
-
-    # alpha = 0.98
-    # acc_metric = RunningAverage(Accuracy())
-    # # acc_metric.attach(trainer, 'running_avg_accuracy')
-
-    evaluator = create_supervised_evaluator(
-        model,
-        metrics={"accuracy": Accuracy(),
-                 "loss": RunningAverage(Accuracy()),
-                 "precision": Precision(average=False),
-                 'recall': Recall(average=False),
-                 'cr': ClassificationReport(output_dict=True, is_multilabel=False)
-                 },
-        device=config.get('device', 'cpu')
+    trainer, train_evaluator, validation_evaluator, test_evaluator, evaluator, pbar = create_trainer(
+        model, optimizer, criterion, lr_scheduler, config, data
     )
+    data_loader, _, _ = data
+    train_loader, train_sampler = data_loader
 
-    # Run model evaluation every 3 epochs and show results
-    @trainer.on(Events.EPOCH_COMPLETED(every=1))
-    def evaluate_model():
-        state = evaluator.run(valid_loader)
-        metrics = state.metrics
-        acc = metrics['accuracy']
-        loss = metrics['loss']
-        #recAll = metrics['recall']
-        #preciss = metrics['precision']
-        #F1 = preciss * recAll * 2 / (preciss + recAll + 1e-20)
-        #F1 = MetricsLambda(lambda t: torch.mean(t).item(), F1)
-        res_dic = metrics['cr']
-        #res_dic = ast.literal_eval(res)
-        pbar.log_message(
-            "Validation set: Average loss: {:.4f}, Accuracy: {:.4f} F1 for class {} is {:.4f}".format(
-                loss, acc, 14, res_dic['14']['f1-score'])
-        )
-        train_evaluator.run(train_loader)
-        validation_evaluator.run(valid_loader)
+    if config['train_now'] is not True:
+        return trainer, model, optimizer, criterion, lr_scheduler, train_loader
 
-        # if idist.get_rank() == 0:
-        #     print(state.metrics)
-
-    # @trainer.on(Events.EPOCH_COMPLETED(every=1))
-    # def log_training_results(engine):
-    #     train_evaluator.run(train_loader)
-        # metrics = train_evaluator.state.metrics
-        # avg_accuracy = metrics['train_acc']
-        # avg_nll_loos = metrics['train_NLLLoss']
-        # pbar.log_message(
-        #     "Training Results - Epoch: {} Avg accuracy: {:.2f} avg loss {:.2f}".format(engine.state.epoch, avg_accuracy,
-        #                                                                                avg_nll_loos)
-        # )
-
-    # @trainer.on(Events.EPOCH_COMPLETED(every=1))
-    # def log_validation_results(engine):
-        # validation_evaluator.run(valid_loader)
-    #     metrics = validation_evaluator.state.metrics
-    #     avg_accuracy = metrics['val_acc']
-    #     avg_nll_loos = metrics['val_NLLLoss']
-    #     pbar.log_message
-    #     (
-    #         "Validation Results - Epoch: {} Avg accuracy: {:.2f} avg loss {:.2f}".format(
-    #             engine.state.epoch, avg_accuracy, avg_nll_loos)
-    #     )
-    #     pbar.n = pbar.last_print_n = 0
-
-    # @ trainer.on(Events.COMPLETED)
-    # def score_function(engine):
-    #     validation_evaluator.run(valid_loader)
-    #     metrics = validation_evaluator.state.metrics
-    #     val_loss = metrics['val_NLLLoss']
-    #     return -val_loss
-
-    # handler_earlyStopping = EarlyStopping(
-        # patience=5, score_function=score_function, trainer=trainer)
-    # validation_evaluator.add_event_handler(
-        # Events.COMPLETED, handler_earlyStopping)
-
-    # tb_logger = common.setup_tb_logging(
-    #     config.get("output_path", "output"), trainer, optimizer, evaluators={"validation": evaluator},
-    # )
-
-    # Setup tensorboard experiment tracking
     if idist.get_rank() == 0:
         # print('Start logging', config['tensorboard_logger_path'])
         tb_logger = common.setup_tb_logging(
@@ -125,6 +39,7 @@ def training(config):
             trainer=trainer, optimizers=optimizer,
             evaluators={"validation": evaluator,
                         # 'cr': evaluator,
+                        # 'loss': trainer,
                         'val_NLLLoss': validation_evaluator,
                         # 'val_acc': validation_evaluator,
                         'train_NLLLoss': train_evaluator,
@@ -132,13 +47,6 @@ def training(config):
                         },
             log_every_iters=1,
         )
-    bestModelSaver = save_best_model_by_val_score(output_path=config['best_model_path'],
-                                                  evaluator=evaluator, model=model,
-                                                  metric_name='accuracy',
-                                                  n_saved=config.get(
-                                                      "best_n_saved", 2)
-                                                  )
-    evaluator.add_event_handler(Events.EPOCH_COMPLETED, bestModelSaver)
 
     trainer.run(train_loader, max_epochs=config.get("max_epochs", 3))
 
