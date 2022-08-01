6090247f39f1f7f1718643b3fc74ac8ae2d2b098
Must-ah <Mustafa.Abdel-Hussein@hotmail.com>
2022-08-01 10:12:23 +0200

    Write the function to file
    Exported the functions from the prev. commit to file.

---
+++ MileStone_0_data_setup_and_exploration/load_plot_img.py
@@ -0,0 +1,22 @@
+import matplotlib.pylab as plt
+
+
+def load_img(img_path: str, img_name: str):
+    """Loads an image from the given path.
+    Args:
+        img_path (str): path to the image
+        img_name (str): name of the image
+    Returns:
+        image (np.array): image as a numpy array
+    """
+    return plt.imread(img_path + '/' + img_name)
+
+
+def plot_img(img: np.array):
+    """Plots an image.
+    Args:
+        img (np.array): image as a numpy array
+    """
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
